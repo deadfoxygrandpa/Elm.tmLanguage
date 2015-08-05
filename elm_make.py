568c39857861edab239ddb3be8083953a892ec9f
Jonathan Toland <toland@dnalot.com>
2015-08-05 11:35:48 -0700

    Fix error parsing
    Parse plain string lines interspersed with JSON from elm-make

--- elm_make.py
+++ elm_make.py
@@ -35,7 +35,8 @@ class ElmMakeCommand(default_exec.ExecCommand):
         finally:
             return super(ElmMakeCommand, cls).__new__(cls)
 
-    def run(self, cmd, working_dir, error_format, **kwargs):
+    def run(self, cmd, working_dir, info_format, error_format, **kwargs):
+        self.info_format = string.Template(info_format)
         self.error_format = string.Template(error_format)
         project = ElmProject(cmd[1])
         cmd[1] = fs.expanduser(project.main_path)
@@ -47,23 +48,20 @@ class ElmMakeCommand(default_exec.ExecCommand):
         else:
             self.debug_text = strings.get('make_highlighting_disabled')
 
-    def on_data(self, proc, json_data):
-        try:
-            error_list = self.decode_json(json_data)
-        except:
-            error_data = json_data
-        else:
-            error_data = '\n'.join(error_list).encode(self.encoding)
-        finally:
-            super(ElmMakeCommand, self).on_data(proc, error_data)
+    def on_data(self, proc, response_data):
+        result_strs = response_data.decode(self.encoding).split('\n')
+        flat_map = lambda f ,xss: sum(map(f, xss), [])
+        output_strs = flat_map(self.format_result, result_strs) + ['']
+        output_data = '\n'.join(output_strs).encode(self.encoding)
+        super(ElmMakeCommand, self).on_data(proc, output_data)
 
-    def decode_json(self, json_data): # throws
-        result_str = json_data.decode(self.encoding)
-        json_str, success_str = result_str.split('\n', 1)
-        decode_error = lambda dict: self.format_error(**dict) if 'type' in dict else dict
-        error_list = json.loads(json_str, object_hook=decode_error)
-        error_list.append(success_str)
-        return error_list
+    def format_result(self, result_str):
+        try:
+            decode_error = lambda dict: self.format_error(**dict) if 'type' in dict else dict
+            return json.loads(result_str, object_hook=decode_error)
+        except:
+            info_str = result_str.strip()
+            return [self.info_format.substitute(info=info_str)] if info_str else []
 
     def format_error(shelf, type, file, region, overview, details, **kwargs):
         line = region['start']['line']
