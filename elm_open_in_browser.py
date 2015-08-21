bb2b19f6a43524a9d34eb6a89b4bed35128cfe4a
Jonathan Toland <toland@dnalot.com>
2015-08-21 02:54:53 -0700

    Vastly simplified Open in Browser command
    Remove plugin integration in favor of standard library.

--- elm_open_in_browser.py
+++ elm_open_in_browser.py
@@ -1,7 +1,15 @@
+import webbrowser
+
 try:     # ST3
+    import urllib.parse as urlparse
+    import urllib.request as urllib
+
     from .elm_plugin import *
     from .elm_project import ElmProject
 except:  # ST2
+    import urlparse
+    import urllib
+
     from elm_plugin import *
     from elm_project import ElmProject
 
@@ -13,26 +21,11 @@ class ElmOpenInBrowserCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
         norm_path = fs.join(self.project.working_dir, fs.expanduser(self.project.html_path))
-        html_path = fs.abspath(norm_path)
-        if not fs.isfile(html_path):
-            sublime.status_message(get_string('open_in_browser.not_found', html_path))
-        elif ElmOpenInBrowserWithSbeCommand.is_patched:
-            self.view.window().run_command('elm_open_in_browser_with_sbe', dict(paths=[html_path]))
-        elif ElmOpenInBrowserWithVibCommand.is_patched:
-            self.view.run_command('elm_open_in_browser_with_vib', dict(path=html_path))
+        file_path = fs.abspath(norm_path)
+        if fs.isfile(file_path):
+            # http://stackoverflow.com/questions/11687478/convert-a-filename-to-a-file-url#comment32679033_14298190
+            file_url = urlparse.urljoin('file:', urllib.pathname2url(file_path))
+            # inspired by https://github.com/noahcoad/open-url
+            webbrowser.open_new_tab(file_url)
         else:
-            sublime.status_message(get_string('open_in_browser.missing_plugin'))
-
-@replace_base_class('SideBarEnhancements.SideBar.SideBarOpenInBrowserCommand')
-class ElmOpenInBrowserWithSbeCommand(sublime_plugin.WindowCommand):
-    pass
-
-@replace_base_class('View In Browser.ViewInBrowserCommand.ViewInBrowserCommand')
-class ElmOpenInBrowserWithVibCommand(sublime_plugin.TextCommand):
-
-    def run(self, edit, path):
-        self.path = path
-        super(ElmOpenInBrowserWithVibCommand, self).run(edit)
-
-    def normalizePath(self, fileToOpen): # override
-        return super(ElmOpenInBrowserWithVibCommand, self).normalizePath(self.path)
+            sublime.status_message(get_string('open_in_browser.not_found', html_path))
