2b6b414e49e956441af26c2ca9203a1b50885f98
Jonathan Toland <toland@dnalot.com>
2015-08-24 00:01:46 -0700

    Reorganize elm-plugin compatibility functions

--- elm_plugin.py
+++ elm_plugin.py
@@ -2,10 +2,18 @@ import sublime
 import sublime_plugin
 import os.path as fs
 
-# MARK: Sublime
+# MARK: compatibility
 
 def is_ST2():
     return sublime.version().startswith('2')
+
+def import_module(path):
+    names = path.split('.')
+    index = 1 if is_ST2() else 0
+    base = __import__(names[index])
+    for name in names[index + 1:]:
+        base = getattr(base, name)
+    return base
 
 def retry_on_main_thread(callback, *args, **kwargs):
     try:
@@ -17,6 +25,8 @@ def retry_on_main_thread(callback, *args, **kwargs):
 def show_quick_panel(window, items, on_select, selected_index=-1, on_highlight=None):
     kwargs = {} if is_ST2() else dict(selected_index=selected_index, on_highlight=on_highlight)
     retry_on_main_thread(window.show_quick_panel, items, on_select, **kwargs)
+
+# MARK: Sublime
 
 def fetch_json(url, callback=None, *args, **kwargs):
     from package_control.clients.json_api_client import JSONApiClient
@@ -43,15 +53,7 @@ def log_string(key, *args):
 
     retry_on_main_thread(on_retry)
 
-# MARK: imports
-
-def import_module(path):
-    names = path.split('.')
-    index = 1 if is_ST2() else 0
-    base = __import__(names[index])
-    for name in names[index + 1:]:
-        base = getattr(base, name)
-    return base
+# MARK: beware thar be dragons below
 
 # defer import as long as possible in case plugin not loaded
 def replace_base_class(path):
@@ -73,15 +75,15 @@ def replace_base_class(path):
                 target_cls.__bases__ = new_bases
 
     def decorator(target_cls):
-        def new(cls, *args, **kwargs):
+        def __new__(cls, *args, **kwargs):
             monkey_patch(target_cls)
             super_ = super(target_cls, cls).__new__
             # TypeError: object() takes no parameters
             return super_(cls) if super_ is object.__new__ else super_(cls, *args, **kwargs)
 
         assert '__new__' not in target_cls.__dict__
-        # ST2: TypeError: unbound method new()
-        target_cls.__new__ = classmethod(new)
+        # ST2: TypeError: unbound method __new__()
+        target_cls.__new__ = classmethod(__new__)
         return target_cls
 
     return decorator
