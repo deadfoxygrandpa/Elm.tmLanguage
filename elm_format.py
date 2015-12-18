f9ec3c98297f58c0ecea38efe77f7274b53738d4
Alex Neslusan <deadfoxygrandpa@gmail.com>
2015-12-18 15:50:33 +0800

    Add basic regex searching

--- elm_format.py
+++ elm_format.py
@@ -1,4 +1,6 @@
 import subprocess
+import os.path
+import re
 import sublime, sublime_plugin
 
 
@@ -6,6 +8,18 @@ class ElmFormatCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		command = "elm-format {} --yes".format(self.view.file_name())
 		p = subprocess.Popen(command, shell=True)
+
+class ElmFormatRegexCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		self.view.window().show_input_panel("regex", "[a-z]", self.find_files, lambda x: x, lambda: None)
+
+	def find_files(self, s):
+		root = self.view.window().folders()[0]
+		for path, subdirs, files in os.walk(root):
+			subdirs[:] = [d for d in subdirs if d not in ["elm-stuff", ".git"]]
+			files[:] = [f for f in files if f.endswith(".elm") and re.match(s, os.path.splitext(f)[0])]
+			for name in files:
+				print(os.path.join(path, name))
 
 class ElmLanguageSupport(sublime_plugin.EventListener):
 	def on_pre_save(self, view):
